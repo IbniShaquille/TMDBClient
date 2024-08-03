2a1f3b91759974d9f3ff49b1c5cff22c02f3a230
Ibni Shaquille Syauqi Ibrahim <ibni.shaquille@ui.ac.id>
2024-08-03 17:07:18 +0700

    feat: new Activity TvShow

---
+++ app/src/main/java/com/dicoding/tmdbclient/presentation/tvshow/TvShowActivity.kt
@@ -0,0 +1,24 @@
+package com.dicoding.tmdbclient.presentation.tvshow
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.dicoding.tmdbclient.R
+import com.dicoding.tmdbclient.databinding.ActivityTvShowBinding
+
+class TvShowActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTvShowBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_tv_show)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
